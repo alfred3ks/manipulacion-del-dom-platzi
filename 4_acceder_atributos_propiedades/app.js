861717078afc93049033a5e61f2109268ebbef01
Alfredo Sánchez <alfred3ks@gmail.com>
2022-03-26 10:38:37 +0100

    Atributos y propiedades de elementos.

---
+++ 4_acceder_atributos_propiedades/app.js
@@ -0,0 +1,12 @@
+// Como acceder a los atributos y propiedades del elemento
+
+let titulo = document.querySelector('#titulo');
+
+// Asi accedemos a los atributos
+console.log(titulo.className);
+console.log(titulo.id);
+console.log(titulo.value);
+
+// Atributos y propiedades
+let input = document.querySelector('#firstName');
+input.value = 'Hola que hace';
